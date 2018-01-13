06729c17a136e8ce24a72b108b7d60446ebadffc
brandon Gulley <brandon.gulley511@gmail.com>
2018-01-13 00:16:06 -0700

    fixed server port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server = express()
 var bp = require('body-parser')
 var DBConnect = require('./config/mlab/mlab-config')
 var cors = require('cors')
-var port = 3000
+var port = "bg-inspire.herokuapp.com"
 
 //route variables
 var todoRoutes = require('./server/routes/todo-routes')
